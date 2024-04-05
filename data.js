014796ad05a753960b6195e386c3b98d25bd1ae0
izhichao <43055550@qq.com>
2024-04-05 16:18:34 +0800

    feat: add samples

--- data.js
+++ data.js
@@ -1,8 +1,40 @@
 const services = {
-  内网服务: [{ text: 'Google', href: 'https://google.com', icon: './icons/google.png', target: '_blank' }],
-  外网服务: [{ text: 'Google', href: 'https://google.com', icon: './icons/google.png' }]
+  搜索引擎: [
+    {
+      text: 'Google',
+      href: 'https://google.com',
+      icon: 'https://cdn.jsdelivr.net/gh/xushier/HD-Icons/border-radius/Google_A.png',
+      target: '_blank'
+    },
+    {
+      text: '百度',
+      href: 'https://baidu.com',
+      icon: 'https://cdn.jsdelivr.net/gh/xushier/HD-Icons/svg/Baidu_A.svg',
+      target: '_blank'
+    }
+  ],
+  网盘: [
+    {
+      text: '百度网盘',
+      href: 'https://pan.baidu.com',
+      icon: 'https://cdn.jsdelivr.net/gh/xushier/HD-Icons/border-radius/Baidudrive_A.png'
+    },
+    {
+      text: '阿里云盘',
+      href: 'https://www.alipan.com',
+      icon: 'https://cdn.jsdelivr.net/gh/xushier/HD-Icons/border-radius/Alidrive_A.png'
+    },
+    {
+      text: '123云盘',
+      href: 'https://www.123pan.com/',
+      icon: 'https://cdn.jsdelivr.net/gh/xushier/HD-Icons/border-radius/123Pan_A.png'
+    }
+  ]
 };
 
 const bookmarks = {
-  书签: [{ text: 'Google', href: 'https://google.com' }]
+  书签: [
+    { text: 'Google', href: 'https://google.com' },
+    { text: '百度', href: 'https://baidu.com' }
+  ]
 };
