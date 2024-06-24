5dfa3905cc8e48720a1e168211337822b243ea5e
izhichao <43055550@qq.com>
2024-06-24 22:10:02 +0800

    chore: update demo

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Homepage Lite —— A very simple homepage
 
-[Demo](https://izhichao.github.io/homepage-lite/)
+[Demo](https://tsx.us.kg)
 
 Download `index.html` `index.css` `data.js`, edit the `data.js` file, `services` corresponds to links with icon and `bookmarks` corresponds to links without icon.The types are as follows:
 
@@ -23,7 +23,7 @@ interface Bookmark {
 
 # Homepage Lite —— 一个非常简陋的引导页
 
-[效果演示](https://izhichao.github.io/homepage-lite/)
+[效果演示](https://tsx.us.kg)
 
 下载 `index.html` `index.css` `data.js`，编辑 `data.js` 文件，其中 `services` 对应的是有 icon 的链接，`bookmarks` 则对应的是无 icon 的链接。其类型如下：
 
--- data.js
+++ data.js
@@ -1,49 +1,67 @@
 const services = {
-  编程: [
+  平台: [
+    {
+      text: 'Blog',
+      href: 'https://zhichao.org',
+      icon: './icons/zhichao.png',
+    },
     {
       text: 'Github',
-      href: 'https://github.com/izhichao/homepage-lite',
+      href: 'https://github.com/izhichao',
       icon: 'https://cdn.jsdelivr.net/gh/xushier/HD-Icons/border-radius/Github_A.png',
-      target: '_blank'
-    }
-  ],
-  搜索引擎: [
-    {
-      text: 'Google',
-      href: 'https://google.com',
-      icon: 'https://cdn.jsdelivr.net/gh/xushier/HD-Icons/border-radius/Google_A.png',
-      target: '_blank'
     },
     {
-      text: '百度',
-      href: 'https://baidu.com',
-      icon: 'https://cdn.jsdelivr.net/gh/xushier/HD-Icons/svg/Baidu_A.svg',
-      target: '_blank'
-    }
-  ],
-  网盘: [
-    {
-      text: '百度网盘',
-      href: 'https://pan.baidu.com',
-      icon: 'https://cdn.jsdelivr.net/gh/xushier/HD-Icons/border-radius/Baidudrive_A.png'
+      text: '知乎',
+      href: 'https://www.zhihu.com/people/24-74-89-56',
+      icon: './icons/zhihu.png',
     },
     {
-      text: '阿里云盘',
-      href: 'https://www.alipan.com',
-      icon: 'https://cdn.jsdelivr.net/gh/xushier/HD-Icons/border-radius/Alidrive_A.png'
+      text: '什么值得买',
+      href: 'https://zhiyou.smzdm.com/member/7192993349',
+      icon: './icons/smzdm.png',
     },
     {
-      text: '123云盘',
-      href: 'https://www.123pan.com/',
-      icon: 'https://cdn.jsdelivr.net/gh/xushier/HD-Icons/border-radius/123Pan_A.png'
+      text: 'CSDN',
+      href: 'https://blog.csdn.net/zc_mk',
+      icon: './icons/csdn.png',
+    },
+    {
+      text: '公众号',
+      href: 'https://zhichao.org/wechat.png',
+      icon: './icons/wechat.png',
     }
   ]
 };
 
 const bookmarks = {
-  书签: [
-    { text: 'Github', href: 'https://github.com/izhichao/homepage-lite' },
-    { text: 'Google', href: 'https://google.com' },
-    { text: '百度', href: 'https://baidu.com' }
+  项目仓库: [
+    { text: 'VitePress 极简主题', href: 'https://github.com/izhichao/vitepress-theme-minimalism' },
+    { text: 'Homepage', href: 'https://github.com/izhichao/homepage-lite' },
+    { text: '氢记账', href: 'https://github.com/izhichao/hydrogen-account' },
+    { text: '互助圈', href: 'https://github.com/izhichao/mutual-aid-zone' },
+    { text: '互助圈(后台)', href: 'https://github.com/izhichao/mutual-aid-zone-admin' },
+  ],
+  项目演示: [
+    { text: 'VitePress 极简主题', href: 'https://vitepress.tsx.us.kg' },
+    { text: 'Homepage', href: 'https://tsx.us.kg' },
+    { text: '氢记账', href: 'https://account.tsx.us.kg' },
+    { text: '互助圈', href: 'https://zone.tsx.us.kg' },
+    { text: '互助圈(后台)', href: 'https://admin.zone.tsx.us.kg' },
   ]
 };
+
+function addTarget(obj) {
+  for (const key in obj) {
+    if (obj.hasOwnProperty(key)) {
+      obj[key].forEach((item) => {
+        if (!('target' in item)) {
+          item.target = '_blank';
+        }
+      });
+    }
+  }
+  return obj;
+}
+
+addTarget(services);
+addTarget(bookmarks);
